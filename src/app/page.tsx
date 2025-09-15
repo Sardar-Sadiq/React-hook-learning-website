4a62d442bef75bd09bb4908c9e276f2d2bf8293a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-16 00:50:10 +0530

    project setup installed successfully

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+    <div className="flex justify-center items-center h-screen bg-red-400 text-5xl">
+      hello world
+    </div>
+   </>
+  );
+}
