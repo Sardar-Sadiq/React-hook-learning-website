931186b42ecb25a583bad9ce40989aab717a4dbf
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-18 23:06:23 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@
 export default function Home() {
   return (
   
-      <h1 className="text-heading-1 font-jost"> Nike  </h1>
+      <h1 className="text-heading-1 font-jost flex justify-center items-center"> Nike  </h1>
   );
 }
