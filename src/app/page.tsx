804f131ea1a51ee65a3f0e114a9539cace72015e
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-20 23:01:18 +0530

    daily commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
   
       <div className="flex h-screen justify-center">
         <h1 className="text-heading-1 font-jost flex justify-center items-center"> Nike  </h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, animi sunt magnam nisi hic doloribus nesciunt iste voluptate obcaecati sequi iure, sapiente maiores in dolore natus ex porro dolores cupiditate.
+        At, consequuntur ea. Nemo itaque enim sapiente temporibus magni obcaecati, ut animi doloribus laboriosam nam est labore inventore quasi quam quas rem sunt quibusdam dignissimos excepturi hic voluptatem consequuntur possimus.
+        In sint beatae vel dolor, officia iusto illum numquam ullam magni! Harum doloremque voluptatibus corporis eum. Dicta eaque numquam quos saepe mollitia sint, delectus, repellendus illum libero qui, optio soluta.
+        Facilis nostrum ipsam, molestiae ab quo molestias, sunt iure rem, illo officiis dolorem rerum odio porro qui voluptatibus. Voluptatibus reprehenderit sit eaque sapiente accusamus illum repudiandae aspernatur adipisci minima nesciunt!
+        Officiis deserunt maiores eveniet quam non voluptates cumque eius deleniti exercitationem velit, et obcaecati sed nobis necessitatibus rerum qui id soluta quisquam ullam beatae perferendis excepturi cupiditate. Id, provident dolor?</p>
       </div>
   );
 }
