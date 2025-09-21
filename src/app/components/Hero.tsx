400df8d1e92f1ba38b904cbee5be836154b3a6ac
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-21 18:43:30 +0530

    changed the project topic

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/app/ui/button'
+import { Input } from '@/app/ui/input'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <Input/>
+      <Button variant="outline">Add </Button>
+    </div>
+  )
+}
+
+export default Hero
